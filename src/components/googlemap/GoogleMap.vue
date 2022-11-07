cfe5d3b90a29830c4f00bae1fe7426704997bf12
Patryk Kowalczyk <noxworl2k@gmail.com>
2022-11-07 10:44:40 +0100

    fix google map

--- src/components/googlemap/GoogleMap.vue
+++ src/components/googlemap/GoogleMap.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="full-width map__flex flex__one">
+  <div class="full-width flex__one">
     <div class="contact__map">
-      <h2>
-        How To reach US
-      </h2>
+      <h2>How To reach US</h2>
       <GMapMap
         :center="center"
         :zoom="12"
         map-type-id="terrain"
-        style="width: 70%; height: 20rem; padding: 5rem;"
+        style="width: 600px; height: 300px; margin: 0 auto"
       >
         <GMapCluster>
           <GMapMarker
@@ -17,14 +15,12 @@
             :position="m.position"
             :clickable="true"
             :draggable="true"
-            @click="center=m.position"
+            @click="center = m.position"
           />
         </GMapCluster>
       </GMapMap>
     </div>
   </div>
-
-
 </template>
 
 <script>
@@ -32,16 +28,16 @@ export default {
   name: "GoogleMap",
   data() {
     return {
-      center: {lat: 52.15733351715033, lng: 20.940302645284405},
+      center: { lat: 52.15733351715033, lng: 20.940302645284405 },
       markers: [
         {
           position: {
-            lat: 52.15733351715033, lng: 20.940302645284405
+            lat: 52.15733351715033,
+            lng: 20.940302645284405,
           },
         },
-      ]
-    }
-  }
+      ],
+    };
+  },
 };
 </script>
-
